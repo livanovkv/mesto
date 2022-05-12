d3279d131cb46763d56c743a56a4051d781b3f02
livanovkv <94444290+livanovkv@users.noreply.github.com>
2022-05-12 20:12:33 +0300

    Исправление ошибки по БЭМ

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,11 @@
 /** Profile */
 export const profileOpenPopupButton = document.querySelector('.profile__edit-button');
-export const editProfileForm = document.querySelector('.popup_profile-form');
+export const editProfileForm = document.querySelector('.popup__form_profile-form');
 
 export const avatarOpenPopupButtonEdit = document.querySelector('.profile__circle');
-export const editAvatarForm = document.querySelector('.popup_avatar-edit-form');
+export const editAvatarForm = document.querySelector('.popup__form_avatar-edit-form');
 
 /** Form add image card */
 export const profileAddButton = document.querySelector('.profile__add-button');
-export const popupAddCardForm = document.querySelector('.popup_add-card-form');
+export const popupAddCardForm = document.querySelector('.popup__form_add-card-form');
 export const cardTemplate = '#card-template';
